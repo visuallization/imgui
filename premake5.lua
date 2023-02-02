9064d15c3123fa1517c67d79311cac9d8192265c
visuallisation <florentin.rieger@gmail.com>
2023-02-02 18:17:23 +0100

    feat(premake): added premake file

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%(prj.name)")
+
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imgui_tables.cpp",
+        "imgui_demo.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+
+    filter { "system.windows", "configurations:Release" }
+        buildoptions "/MT"
